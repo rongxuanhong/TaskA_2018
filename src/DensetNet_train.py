1d72e7653f20eab5bda1829535c966b666a3a71b
root <root@ccyounghrx.c.genial-current-212110.internal>
2018-10-06 13:05:04 +0000

    u

--- src/DensetNet_train.py
+++ src/DensetNet_train.py
@@ -166,8 +166,8 @@ def run_task_eager(args):
     step_counter = tf.train.get_or_create_global_step()
     # learning_rate = tf.train.piecewise_constant(step_counter, [int(0.4 * args.epochs), int(0.75 * args.epochs)],
     #                                             [args.lr, args.lr * 0.1, args.lr * 0.01])
-    optimizer = tf.train.AdamOptimizer()
-    # optimizer = tf.train.MomentumOptimizer(args.lr, momentum=0.9, use_nesterov=True)
+   # optimizer = tf.train.AdamOptimizer()
+    optimizer = tf.train.MomentumOptimizer(args.lr, momentum=0.9, use_nesterov=True)
 
     # 5. 创建用于写入tensorboard总结的文件写入器
     if args.output_dir:
