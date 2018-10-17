9dc124641d258261e454e99adbed1ddb2507f580
rongxuanhong <389817374@qq.com>
2018-10-17 09:25:43 +0800

    u

--- src/DensetNet_train.py
+++ src/DensetNet_train.py
@@ -104,6 +104,7 @@ def train(model, optimizer, dataset, step_counter, total_batch, args):
 
                 # 计算损失
                 l2_loss = tf.add_n(model.losses)
+                print("l2_loss:",l2_loss)
                 loss_value = loss(logits, labels) + l2_loss
                 # loss_value = lam * loss(logits, label_a) + (1 - lam) * loss(logits, label_b) + l2_loss
                 # 每10步记录日志
@@ -188,10 +189,10 @@ def run_task_eager(args):
 
     step_counter = tf.train.get_or_create_global_step()
 
-    learning_rate = tf.train.piecewise_constant(step_counter, [15, 22],
-                                                [args.lr, args.lr * 0.1, args.lr * 0.01, ])
-    # optimizer = tf.train.AdamOptimizer()
-    optimizer = tf.train.MomentumOptimizer(learning_rate, momentum=0.9, use_nesterov=True)
+    # learning_rate = tf.train.piecewise_constant(step_counter, [15, 22],
+    #                                             [args.lr, args.lr * 0.1, args.lr * 0.01, ])
+    optimizer = tf.train.AdamOptimizer()
+    # optimizer = tf.train.MomentumOptimizer(learning_rate, momentum=0.9, use_nesterov=True)
     # learing_rate2 = tf.train.exponential_decay(learning_rate=args.lr, global_step=step_counter, decay_steps=args.epochs, decay_rate=0.9,
     #                                            staircase=True)
     # learning_rate = tf.train.piecewise_constant(step_counter, [int(0.4 * args.epochs), int(0.75 * args.epochs)],
