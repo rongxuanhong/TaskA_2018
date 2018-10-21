03a3b4548e2a5c0bd486f8e2822b6907c7d1efb7
rongxuanhong <389817374@qq.com>
2018-10-21 09:12:32 +0800

    1917

--- src/run_tf_dense_net_server.sh
+++ src/run_tf_dense_net_server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 python3 DensetNet_train.py --batch_size=64 --epochs=2 --nb_layers=40 --n_db=3 --grow_rate=24 \
---data_format='channels_last' --output_dir='/data/taska_2018/src/check_point' --lr=0.0001 --log_interval=10 --alpha=0.2
+--data_format='channels_last' --output_dir='/data/TaskA_2018/src/check_point' --lr=0.0001 --log_interval=10 --alpha=0.2
 
