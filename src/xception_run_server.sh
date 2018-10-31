47967cd9f46c1f3d2a35bd27dfe284dcc0839bf3
rongxuanhong <389817374@qq.com>
2018-10-31 10:07:40 +0800

    u

--- src/xception_run_server.sh
+++ src/xception_run_server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-python3 Xception_train.py --batch_size=64 --epochs=10 --output_dir='/data/TaskA_2018/src/check_point' --lr=0.01 --log_interval=10 --alpha=0.2
+python3 Xception_train.py --batch_size=60 --epochs=10 --output_dir='/data/TaskA_2018/src/check_point' --lr=0.01 --log_interval=10 --alpha=0.2
 
