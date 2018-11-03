1ed628e2a2e73a51f1ed044be941ae2937b8d66b
rongxuanhong <389817374@qq.com>
2018-11-03 20:30:36 +0800

    u

--- src/Xception_train.py
+++ src/Xception_train.py
@@ -197,7 +197,7 @@ def run_task_eager(args):
     create_folder(check_point_prefix)
 
     check_point = tf.train.Checkpoint(model=model, optimizer=optimizer, step_counter=step_counter)
-    check_point.restore(os.path.join(args.output_dir, 'model3', 'cpkt-10'))  # 存在就恢复模型(可不使用)
+    check_point.restore(os.path.join(args.output_dir, 'model3', 'cpkt-13'))  # 存在就恢复模型(可不使用)
     # check_point.restore(tf.train.latest_checkpoint(os.path.join(args.output_dir, 'model3')))
     # 7. 训练、评估
     # with tf.device(device):
