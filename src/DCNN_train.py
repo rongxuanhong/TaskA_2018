8eb5f43c4c7626d5a531786d92f6ae6f0a118d8a
rongxuanhong <389817374@qq.com>
2018-11-11 16:49:13 +0800

    u

--- src/DCNN_train.py
+++ src/DCNN_train.py
@@ -178,7 +178,8 @@ def run_task_eager(args):
 
     # learning_rate = tf.train.piecewise_constant(step_counter, boundaries=boundaries, values=learning_rates)
     # learning_rate = tf.train.polynomial_decay(0.001, step_counter, 50, end_learning_rate=0, power=1.0)
-    optimizer = tf.train.AdamOptimizer()
+    # print(learning_rate)
+    optimizer = tf.train.AdamOptimizer(0.001 * 0.2)
     # learning_rate = tf.train.exponential_decay(learning_rate=args.lr, global_step=step_counter, decay_steps=2,
     #                                            decay_rate=0.5,
     # #                                            staircase=False)
@@ -206,11 +207,11 @@ def run_task_eager(args):
 
     check_point = tf.train.Checkpoint(model=model, optimizer=optimizer, step_counter=step_counter)
     # check_point.restore(os.path.join(args.output_dir, 'model3', 'cpkt-34'))  # 存在就恢复模型(可不使用)
-    # check_point.restore(tf.train.latest_checkpoint(os.path.join(args.output_dir, 'model3')))
+    check_point.restore(tf.train.latest_checkpoint(os.path.join(args.output_dir, 'model3')))
     # 7. 训练、评估
     # with tf.device(device):
     start_time = datetime.now()
-    max_acc = 0
+    max_acc = 60.0
     for i in range(args.epochs):  # 迭代的轮次
         with summary_writer.as_default():
             # 训练
