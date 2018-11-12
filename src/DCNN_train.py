ce7d00ca7942163453321c810b8f3b16de027482
rongxuanhong <389817374@qq.com>
2018-11-12 21:29:32 +0800

    u

--- src/DCNN_train.py
+++ src/DCNN_train.py
@@ -186,7 +186,7 @@ def run_task_eager(args):
     # learning_rate = tf.train.exponential_decay(
     #     learning_rate=0.001, global_step=step_counter, decay_steps=2, decay_rate=0.9, staircase=True)
     # #
-    optimizer = tf.train.MomentumOptimizer(0.5, momentum=0.9, use_nesterov=True)
+    optimizer = tf.train.MomentumOptimizer(0.001, momentum=0.9, use_nesterov=True)
     # learning_rate = tf.train.piecewise_constant(step_counter, [int(0.4 * args.epochs), int(0.75 * args.epochs)],
     #                                             [args.lr, args.lr * 0.1, args.lr * 0.01])
 
@@ -206,12 +206,12 @@ def run_task_eager(args):
     create_folder(check_point_prefix)
 
     check_point = tf.train.Checkpoint(model=model, optimizer=optimizer, step_counter=step_counter)
-    # check_point.restore(os.path.join(args.output_dir, 'model3', 'cpkt-34'))  # 存在就恢复模型(可不使用)
-    check_point.restore(tf.train.latest_checkpoint(os.path.join(args.output_dir, 'model3')))
+    check_point.restore(os.path.join(args.output_dir, 'model3', 'cpkt-10'))  # 存在就恢复模型(可不使用)
+    # check_point.restore(tf.train.latest_checkpoint(os.path.join(args.output_dir, 'model3')))
     # 7. 训练、评估
     # with tf.device(device):
     start_time = datetime.now()
-    max_acc = 0
+    max_acc = 54.5
     for i in range(args.epochs):  # 迭代的轮次
         with summary_writer.as_default():
             # 训练
