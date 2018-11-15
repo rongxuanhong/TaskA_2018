ca92e95ead67a2d1286c5d27d82b3dba4d8132e0
rongxuanhong <389817374@qq.com>
2018-11-15 18:44:27 +0800

    u

--- src/DCNN_train.py
+++ src/DCNN_train.py
@@ -183,14 +183,14 @@ def run_task_eager(args):
     #     learning_rate=0.0007, global_step=step_counter, decay_steps=1, decay_rate=0.9, )
     # optimizer = tf.train.AdamOptimizer()
 
-    optimizer = tf.train.MomentumOptimizer(0.0001, momentum=0.9, use_nesterov=True)
+    optimizer = tf.train.MomentumOptimizer(0.01, momentum=0.9, use_nesterov=True)
     # learning_rate = tf.train.piecewise_constant(step_counter, [int(0.4 * args.epochs), int(0.75 * args.epochs)],
     #                                             [args.lr, args.lr * 0.1, args.lr * 0.01])
 
     # 5. 创建用于写入tensorboard总结的文件写入器
     if args.output_dir:
-        train_dir = os.path.join(args.output_dir, 'model4', 'train')
-        test_dir = os.path.join(args.output_dir, 'model4', 'test')
+        train_dir = os.path.join(args.output_dir, 'model3', 'train')
+        test_dir = os.path.join(args.output_dir, 'model3', 'test')
         tf.gfile.MakeDirs(args.output_dir)  # 创建所有文件
     else:
         train_dir = None
@@ -199,11 +199,11 @@ def run_task_eager(args):
     test_summary_writer = tfc.summary.create_file_writer(test_dir, flush_millis=10000, name='test')
 
     # 6. 创建或者恢复checkpoint
-    check_point_prefix = os.path.join(args.output_dir, 'model4', 'cpkt')
+    check_point_prefix = os.path.join(args.output_dir, 'model3', 'cpkt')
     create_folder(check_point_prefix)
 
     check_point = tf.train.Checkpoint(model=model, optimizer=optimizer, step_counter=step_counter)
-    check_point.restore(os.path.join(args.output_dir, 'model4', 'cpkt-41'))  # 存在就恢复模型(可不使用)
+    check_point.restore(os.path.join(args.output_dir, 'model3', 'cpkt-16'))  # 存在就恢复模型(可不使用)
     # check_point.restore(tf.train.latest_checkpoint(os.path.join(args.output_dir, 'model4')))
     # 7. 训练、评估
     # with tf.device(device):
